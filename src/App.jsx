3f615849e4341a3db420f4874879d5b18104d00e
Montragor <antonio.panariello@web.de>
2026-04-15 09:48:14 +0200

    Ergänze Seitenlayout und GoalsCard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import LessonList from './components/LessonList'
 import { lessons } from './data/lessons'
+import GoalsCard from './components/GoalsCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,14 @@ function App() {
       <div className="min-h-screen bg-slate-950 text-slate-100">
         <div className="mx-auto flex max-w-7xl flex-col gap-8 px-4 py-8 sm:px-6 lg:px-8">
           <Header />
-          <LessonList lessons={lessons} />
+
+         <main className="grid gap-6 lg:grid-cols-[2fr_1fr]">   
+             <LessonList lessons={lessons} />
+             <div>
+              <GoalsCard />
+             </div>
+          </main>
+
         </div>
       </div>
     </>
